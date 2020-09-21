64e5deca061034ac95dbff1a50f72affb629e51b
matthewsteedman <msteedman77@gmail.com>
2020-09-21 11:06:13 +0200

    First day's work

---
+++ day1.py
@@ -0,0 +1,9 @@
+print("Hello world")
+
+class_name = "Class2"
+
+print("I was coded in " + class_name)
+
+class_students = "20"
+
+print("There are " + class_students + " students in " + class_name)
