ad956221f8fc125e63bc74084934c4414c050139
matthewsteedman <msteedman77@gmail.com>
2020-09-21 13:21:54 +0200

    First days work

--- day1.py
+++ day1.py
@@ -7,3 +7,21 @@ print("I was coded in " + class_name)
 class_students = "20"
 
 print("There are " + class_students + " students in " + class_name)
+
+student_name = input("What is your name: ")
+
+print("So your name is: " + student_name)
+
+student_age = int(input("How old are you: "))
+
+print("So in 10 years you'll be ... ")
+print (student_age + 10)
+
+you = input("How are you: ") 
+print(you)
+
+radius = int(input("Radius: "))
+pi= 3.145
+
+circle = (pi * radius * radius)
+print (circle)
---
+++ day1_exercises.py
@@ -0,0 +1,10 @@
+#Task 1 accept an input and display
+# Hello (name) How are you?
+# Where (name) is the input
+
+
+# TASK 2 accept an input of the radius of a circle and print the circumference
+# Example
+# Raduis: 5 / input 
+# Circumference: 3.145 // output
+# Format 2radius 3.145 
